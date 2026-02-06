3c0b11adfeb19ce47a476cefdb496565a18e1db2
Daniel Reis <dreis@opensourceintegrators.com>
2026-02-06 17:03:20 +0000

    [FIX] sale_parner_address_filter: select any partner was not working

--- sale_partner_address_filter/models/sale_order.py
+++ sale_partner_address_filter/models/sale_order.py
@@ -1,16 +1,11 @@
 # Copyright 2025 Open Source Integrators
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
-
-    commercial_partner_id = fields.Many2one(
-        related="partner_id.commercial_partner_id",
-        readonly=True,
-    )
 
     only_customer_addresses = fields.Boolean(
         string="Select Only Customer Contacts",
@@ -18,3 +13,18 @@ class SaleOrder(models.Model):
         help="When checked, delivery and invoice addresses are filtered "
         "to only show addresses belonging to the customer's commercial company.",
     )
+
+    @api.depends("only_customer_addresses", "partner_id.commercial_partner_id")
+    def _compute_address_domain(self):
+        """Compute domain for address fields"""
+        for order in self:
+            comm_partner = order.partner_id.commercial_partner_id
+            address_domain = (
+                [("commercial_partner_id", "=", comm_partner.id)]
+                if order.only_customer_addresses
+                else []
+            )
+            # Return domain as string for XML compatibility
+            order.address_domain = str(address_domain)
+
+    address_domain = fields.Text(compute="_compute_address_domain")
